427f77aa9d118bf6b6817950e210d53362888c26
dom <dom@dc7.com.br>
2025-05-21 22:55:25 -0300

    correção total

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './style.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RegistroVendaPage from './pages/RegistroVendaPage';
+import RegistroCortesiaPage from './pages/RegistroCortesiaPage';
+import CheckInPage from './pages/CheckInPage';
+import RelatorioPage from './pages/RelatorioPage';
+import MenuPrincipal from './pages/MenuPrincipal';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Festa Junina - Painel de Check-in</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MenuPrincipal />} />
+        <Route path="/registro" element={<RegistroVendaPage />} />
+        <Route path="/cortesia" element={<RegistroCortesiaPage />} />
+        <Route path="/checkin" element={<CheckInPage />} />
+        <Route path="/relatorios" element={<RelatorioPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
